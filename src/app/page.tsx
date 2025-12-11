32004a4a1e4ce163255e4f1c1d0958378e75ae60
Benaya Josua <benayajosua@192.168.1.101>
2025-12-11 17:18:42 +0700

    Testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Home() {
   return (
     <div>
-      <h1>Welcome to My portfolio</h1>
+      <h1>Welcome to My portfolio v2</h1>
     </div>
   );
 }
