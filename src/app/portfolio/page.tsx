d78e5565e8846c59c8a89960103262ca04c235ae
Benaya Josua <benayajosua@192.168.1.100>
2025-12-12 22:50:14 +0700

    feat : buat page portfolio

---
+++ src/app/portfolio/page.tsx
@@ -0,0 +1,44 @@
+"use client"
+
+import NavigationBar from "@/components/Navbar"
+import Footer from "@/components/Footer"
+import { Montserrat } from "next/font/google"
+import DecryptedText from "@/decoration/DecryptedText"
+
+const monserratFont = Montserrat({
+    subsets: ["latin"],
+    weight: "300",
+})
+
+export default function HomePage() {
+    return (
+        <div className={`${monserratFont.className}`}>
+            
+            {/* navbar */}
+            <div className="fixed top-0 left-0 w-full z-10">
+                <NavigationBar />
+            </div>
+
+            {/* konten utama */}
+            <div className="h-screen pt-20 flex flex-col bg-white">
+                <div className="flex flex-1/2 justify-center items-center">
+                    <div className="text-4xl font-thin">
+                        <DecryptedText
+                            text="Coming Soon"
+                            speed={100}
+                            maxIterations={30}
+                            characters="qwertyuiop[]asdfghjkl;'zxcvbnm,./1234567890-="
+                            className="revealed"
+                            parentClassName="all-letters"
+                            encryptedClassName="encrypted"
+                            animateOn="view"
+                        />
+                    </div>
+                </div>
+            </div>
+
+            {/* footer */}
+            <Footer />
+        </div>
+    )
+}
