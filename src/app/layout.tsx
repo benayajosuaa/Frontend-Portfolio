9bd43fdaf49079fda11c178e28e8ad3c29fde149
benaya josua <benaya.josua@gmail.com>
2026-02-21 18:27:43 +0700

    add : judul utk SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "halobenaya",
+  title: "halobenaya - Benaya Josua",
   description: "Benaya Portfolio",
   icons: {
     icon: "/pagar.png",
